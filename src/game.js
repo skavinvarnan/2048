a5a700e2ba3bd887db2841fb642833ceecbd8060
Kavin Varnan <skavinvarnan@gmail.com>
2019-03-20 21:25:13 +0530

    Finding available spots on the matrix

--- src/game.js
+++ src/game.js
@@ -4,6 +4,31 @@
 //  Created by Kavin Varnan on 2019-03-20
 //
 
+// Hint
+//   [1, 2, 3, 4],
+//   [5, 6, 7, 8],
+//   [9, 10, 11, 12],
+//   [13, 14, 15, 16]
+//
+// is in matrix element as [x, y]
+//
+//   [0, 0] [0, 1] [0, 2] [0, 3]
+//   [1, 0] [1, 1] [1, 2] [1, 3]
+//   [2, 0] [2, 1] [2, 2] [2, 3]
+//   [3, 0] [3, 1] [3, 2] [3, 3]
+//
+// As table by console.table(matrix)
+// ┌─────────┬────┬────┬────┬────┐
+// │ (index) │ 0  │ 1  │ 2  │ 3  │
+// ├─────────┼────┼────┼────┼────┤
+// │    0    │ 1  │ 2  │ 3  │ 4  │
+// │    1    │ 5  │ 6  │ 7  │ 8  │
+// │    2    │ 9  │ 10 │ 11 │ 12 │
+// │    3    │ 13 │ 14 │ 15 │ 16 │
+// └─────────┴────┴────┴────┴────┘
+//
+
+
 class Game {
 
   constructor() {
@@ -31,9 +56,23 @@ class Game {
     return colArray;
   };
 
+  // To find the available spots for numbers to be inserted
+  findAvailableSpots(matrix) {
+    let availableSpots = [];
+    for (let i = 0; i < this.rowLength; i++) {
+      for (let j = 0; j < this.colLength; j++) {
+        if (matrix[i][j] === -1) {
+          availableSpots.push([{x: i, y: j}]);
+        }
+      }
+    }
+    return availableSpots;
+  }
+
   run() {
     this.matrix = this.generateInitialMatrix();
-    console.table(this.matrix)
+    const availableSpots = this.findAvailableSpots(this.matrix);
+    console.table(availableSpots)
   }
   
 }
--- test/game.js
+++ test/game.js
@@ -10,8 +10,172 @@ const { expect } = require('chai');
 
 
 describe('Game test', function () {
-  it('Test generateInitialMatrix', function () {
+  it('generateInitialMatrix', () => {
     const initialMatrix = new Game().generateInitialMatrix();
     expect(initialMatrix).to.eql([[-1, -1, -1, -1], [-1, -1, -1, -1], [-1, -1, -1, -1], [-1, -1, -1, -1]]);
   });
+
+  it('findAvailableSpots pos [0, 0]', () => {
+    const matrix = [
+      [5, -1, -1, -1],
+      [-1, -1, -1, -1],
+      [-1, -1, -1, -1],
+      [-1, -1, -1, -1]
+    ];
+    const availableSpots = new Game().findAvailableSpots(matrix);
+    expect(availableSpots).to.eql([
+      [{x: 0, y: 1}],
+      [{x: 0, y: 2}],
+      [{x: 0, y: 3}],
+      [{x: 1, y: 0}],
+      [{x: 1, y: 1}],
+      [{x: 1, y: 2}],
+      [{x: 1, y: 3}],
+      [{x: 2, y: 0}],
+      [{x: 2, y: 1}],
+      [{x: 2, y: 2}],
+      [{x: 2, y: 3}],
+      [{x: 3, y: 0}],
+      [{x: 3, y: 1}],
+      [{x: 3, y: 2}],
+      [{x: 3, y: 3}],
+    ])
+  });
+
+  it('findAvailableSpots pos [0, 2]', () => {
+    const matrix = [
+      [-1, -1, 6, -1],
+      [-1, -1, -1, -1],
+      [-1, -1, -1, -1],
+      [-1, -1, -1, -1]
+    ];
+    const availableSpots = new Game().findAvailableSpots(matrix);
+    expect(availableSpots).to.eql([
+      [{x: 0, y: 0}],
+      [{x: 0, y: 1}],
+      [{x: 0, y: 3}],
+      [{x: 1, y: 0}],
+      [{x: 1, y: 1}],
+      [{x: 1, y: 2}],
+      [{x: 1, y: 3}],
+      [{x: 2, y: 0}],
+      [{x: 2, y: 1}],
+      [{x: 2, y: 2}],
+      [{x: 2, y: 3}],
+      [{x: 3, y: 0}],
+      [{x: 3, y: 1}],
+      [{x: 3, y: 2}],
+      [{x: 3, y: 3}],
+    ])
+  });
+
+  it('findAvailableSpots pos [1, 3]', () => {
+    const matrix = [
+      [-1, -1, -1, -1],
+      [-1, -1, -1, 6],
+      [-1, -1, -1, -1],
+      [-1, -1, -1, -1]
+    ];
+    const availableSpots = new Game().findAvailableSpots(matrix);
+    expect(availableSpots).to.eql([
+      [{x: 0, y: 0}],
+      [{x: 0, y: 1}],
+      [{x: 0, y: 2}],
+      [{x: 0, y: 3}],
+      [{x: 1, y: 0}],
+      [{x: 1, y: 1}],
+      [{x: 1, y: 2}],
+      [{x: 2, y: 0}],
+      [{x: 2, y: 1}],
+      [{x: 2, y: 2}],
+      [{x: 2, y: 3}],
+      [{x: 3, y: 0}],
+      [{x: 3, y: 1}],
+      [{x: 3, y: 2}],
+      [{x: 3, y: 3}],
+    ])
+  });
+
+  it('findAvailableSpots pos [2, 1]', () => {
+    const matrix = [
+      [-1, -1, -1, -1],
+      [-1, -1, -1, -1],
+      [-1, 5, -1, -1],
+      [-1, -1, -1, -1]
+    ];
+    const availableSpots = new Game().findAvailableSpots(matrix);
+    expect(availableSpots).to.eql([
+      [{x: 0, y: 0}],
+      [{x: 0, y: 1}],
+      [{x: 0, y: 2}],
+      [{x: 0, y: 3}],
+      [{x: 1, y: 0}],
+      [{x: 1, y: 1}],
+      [{x: 1, y: 2}],
+      [{x: 1, y: 3}],
+      [{x: 2, y: 0}],
+      [{x: 2, y: 2}],
+      [{x: 2, y: 3}],
+      [{x: 3, y: 0}],
+      [{x: 3, y: 1}],
+      [{x: 3, y: 2}],
+      [{x: 3, y: 3}],
+    ])
+  });
+
+  it('findAvailableSpots pos [3, 0]', () => {
+    const matrix = [
+      [-1, -1, -1, -1],
+      [-1, -1, -1, -1],
+      [-1, -1, -1, -1],
+      [4, -1, -1, -1]
+    ];
+    const availableSpots = new Game().findAvailableSpots(matrix);
+    expect(availableSpots).to.eql([
+      [{x: 0, y: 0}],
+      [{x: 0, y: 1}],
+      [{x: 0, y: 2}],
+      [{x: 0, y: 3}],
+      [{x: 1, y: 0}],
+      [{x: 1, y: 1}],
+      [{x: 1, y: 2}],
+      [{x: 1, y: 3}],
+      [{x: 2, y: 0}],
+      [{x: 2, y: 1}],
+      [{x: 2, y: 2}],
+      [{x: 2, y: 3}],
+      [{x: 3, y: 1}],
+      [{x: 3, y: 2}],
+      [{x: 3, y: 3}],
+    ])
+  });
+
+  it('findAvailableSpots pos [1, 2]', () => {
+    const matrix = [
+      [-1, -1, -1, -1],
+      [-1, -1, 6, -1],
+      [-1, -1, -1, -1],
+      [-1, -1, -1, -1]
+    ];
+    const availableSpots = new Game().findAvailableSpots(matrix);
+    expect(availableSpots).to.eql([
+      [{x: 0, y: 0}],
+      [{x: 0, y: 1}],
+      [{x: 0, y: 2}],
+      [{x: 0, y: 3}],
+      [{x: 1, y: 0}],
+      [{x: 1, y: 1}],
+      [{x: 1, y: 3}],
+      [{x: 2, y: 0}],
+      [{x: 2, y: 1}],
+      [{x: 2, y: 2}],
+      [{x: 2, y: 3}],
+      [{x: 3, y: 0}],
+      [{x: 3, y: 1}],
+      [{x: 3, y: 2}],
+      [{x: 3, y: 3}],
+    ])
+  });
+
+
 });
