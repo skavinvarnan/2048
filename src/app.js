557b37109f09ddb24f5d3daa73291d23d0aaee63
Kavin Varnan <skavinvarnan@gmail.com>
2019-03-22 11:18:52 +0530

    Adding keypress dependency to create user interaction

--- src/app.js
+++ src/app.js
@@ -4,7 +4,38 @@
 //  Created by Kavin Varnan on 2019-03-20
 //
 
-const Game = require('./game');
+// const Game = require('./game');
+//
+// new Game().run();
 
-new Game().run();
+const keypress = require('keypress');
 
+// make `process.stdin` begin emitting "keypress" events
+keypress(process.stdin);
+
+// listen for the "keypress" event
+process.stdin.on('keypress', (ch, key) => {
+  if (key && key.ctrl && key.name === 'c') {
+    process.stdin.pause();
+    console.log("Exit game");
+  }
+
+  if (key && key.name === 'right') {
+    console.log("Slide Right");
+  }
+
+  if (key && key.name === 'left') {
+    console.log("Slide Left");
+  }
+
+  if (key && key.name === 'up') {
+    console.log("Slide Up");
+  }
+
+  if (key && key.name === 'down') {
+    console.log("Slide Down");
+  }
+});
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
