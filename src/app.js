02c4b6d76d4a9908b0c23107ed5e2b126405e3d9
Kavin Varnan <skavinvarnan@gmail.com>
2019-03-22 13:22:32 +0530

    Using cli-table3 for neat representation of table

--- src/app.js
+++ src/app.js
@@ -6,50 +6,75 @@
 
 const Game = require('./game');
 const Keypress = require('./keypress');
+const Table = require('cli-table3');
 
 class App {
   constructor() {
     this.game = new Game();
+    // Create the initial 0 matrix
     this.game.generateInitialMatrix();
 
     const availableSpots = this.game.findAvailableSpots(this.game.matrix);
     const spot1 = this.game.getRandomSpot(availableSpots);
     const spot2 = this.game.getRandomSpot(availableSpots);
+
+    // Insert two number in random two spots
     this.game.insertRandomNumberToMatrixForSpot(spot1);
     this.game.insertRandomNumberToMatrixForSpot(spot2);
-
-    console.table(this.game.matrix);
+    this.renderTable();
     new Keypress(this.upClicked.bind(this), this.downClicked.bind(this), this.leftClicked.bind(this), this.rightClicked.bind(this))
   }
 
+  // Up click action
   upClicked() {
     this.game.slideUp(this.game.matrix);
     this.insertNumberOnRandomSpot();
-    console.table(this.game.matrix);
+    this.renderTable();
   }
 
+  // Down click action
   downClicked() {
     this.game.slideDown(this.game.matrix);
     this.insertNumberOnRandomSpot();
-    console.table(this.game.matrix);
+    this.renderTable();
   }
 
+  // Left click action
   leftClicked() {
     this.game.slideLeft(this.game.matrix);
     this.insertNumberOnRandomSpot();
-    console.table(this.game.matrix);
+    this.renderTable();
   }
 
+  // Right click action
   rightClicked() {
     this.game.slideRight(this.game.matrix);
     this.insertNumberOnRandomSpot();
-    console.table(this.game.matrix);
+    this.renderTable();
   }
 
+  // After a move insert either 2 or 4 on a random available spot
   insertNumberOnRandomSpot() {
     const availableSpots = this.game.findAvailableSpots(this.game.matrix);
     const randomStop = this.game.getRandomSpot(availableSpots);
     this.game.insertRandomNumberToMatrixForSpot(randomStop);
+  }
+
+  renderTable() {
+    // Recreate table everytime we render
+    const table = new Table({
+      colWidths: [6, 6, 6, 6],
+      rowHeights: [3, 3, 3, 3],
+      colAligns: ['center', 'center', 'center', 'center'],
+      rowAligns: ['center', 'center', 'center', 'center'],
+      chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+        , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
+        , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
+        , 'right': '║' , 'right-mid': '╢' , 'middle': '│' }
+    });
+    // Use spread operator to convert
+    table.push(...this.game.matrix);
+    console.log(table.toString());
   }
 }
 
